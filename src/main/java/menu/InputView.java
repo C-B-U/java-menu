70acdba5a5ad858b279829afcd3463a8b326ae20
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-23 01:34:06 +0900

    feat(InputView): 코치 이름, 못먹는 음식 입력받는 메서드 구현

---
+++ src/main/java/menu/InputView.java
@@ -0,0 +1,52 @@
+package menu;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+    private static final OutputView outputView = new OutputView();
+    private static final InputValidator inputValidator = new InputValidator();
+    private static final String COMMA = ",";
+
+    public List<String> readCoachNames() {
+        String names;
+        do {
+            names = inputCoachNames();
+        } while (names == null);
+        return Arrays.asList(names.split(COMMA));
+    }
+
+    private String inputCoachNames() {
+        String input;
+        try {
+            input = Console.readLine();
+            inputValidator.validateCoachNames(input);
+        } catch (IllegalArgumentException exception) {
+            outputView.printExceptionMessage(exception);
+            return null;
+        }
+        return input;
+    }
+
+    public List<String> readHateFood() {
+        String foods;
+        do {
+            foods = inputHateFood();
+        } while (foods == null);
+        return Arrays.asList(foods.split(COMMA));
+    }
+
+    private String inputHateFood() {
+        String input;
+        try {
+            input = Console.readLine();
+            inputValidator.validateFoods(input);
+        } catch (IllegalArgumentException exception) {
+            outputView.printExceptionMessage(exception);
+            return null;
+        }
+        return input;
+    }
+}
