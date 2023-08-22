895d13705874f5cae063494908e221d7bed6b72f
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-23 01:38:48 +0900

    feat(MenuMap): 추천 메뉴 맵을 그려주는 클래스 생성

---
+++ src/main/java/menu/MenuMap.java
@@ -0,0 +1,36 @@
+package menu;
+
+import java.util.*;
+
+public class MenuMap {
+    private final LinkedHashMap<String, List<String>> map = new LinkedHashMap<>();
+    private static final int FIRST_INDEX = 0;
+    private String resultMap = "";
+
+    public MenuMap (List<Coach> information) {
+        information.forEach(info -> map.put(info.getName(), new ArrayList<>()));
+    }
+
+    public void add(String name, String recommend) {
+        map.get(name).add(recommend);
+    }
+
+    public boolean isDuplicate(String name, String recommend) {
+        return map.get(name).contains(recommend);
+    }
+
+    public String draw() {
+        map.forEach((name, menus) -> {
+            menus.add(FIRST_INDEX, name);
+            resultMap += join(menus);
+        });
+        return resultMap;
+    }
+
+    private String join(List<String> element) {
+        return MapElement.OPEN_BRACKET.get()
+                + String.join(MapElement.DELIMITER.get(), element)
+                + MapElement.CLOSE_BRACKET.get()
+                + MapElement.NEW_LINE.get();
+    }
+}
