8f423a8c40e77e2f9866f9aac2f979a043cb0170
GaBaljaintheroom <junsu1222@naver.com>
2023-08-30 23:21:01 +0900

    test: 추천된 메뉴가 코치 수 만큼 저장되는지 확인

---
+++ src/test/java/menu/MenuTest.java
@@ -0,0 +1,25 @@
+package menu;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+
+class MenuTest {
+
+    CoachNames coachNames = new CoachNames("토미,제임스,포코");
+    ForbiddenMenu forbiddenMenu = new ForbiddenMenu();
+
+
+
+    @Test
+    @DisplayName("추천된 메뉴가 코치 수 만큼 저장되는지 확인한다.")
+    void checkSaveRecommendMenus(){
+        RecommendMenu recommendMenu = new RecommendMenu();
+        RecommendCategory recommendCategory = new RecommendCategory();
+        recommendCategory.recommendCategory();
+        recommendMenu.recommendMenu(coachNames, forbiddenMenu, recommendCategory);
+
+        assertThat(recommendMenu.getMenus().size()).isEqualTo(3);
+    }
+}
