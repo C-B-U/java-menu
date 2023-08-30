aa0bc6f207aa24eb87cc3caa6844e0908732773a
GaBaljaintheroom <junsu1222@naver.com>
2023-08-31 01:25:47 +0900

    feat: OutputElement 생성

---
+++ src/main/java/menu/OutputElement.java
@@ -0,0 +1,20 @@
+package menu;
+
+public enum OutputElement {
+    FIRST_ELEMENT("[ "),
+    MIDDLE_ELEMENT(" | "),
+    LAST_ELEMENT(" ]"),
+    NEXT_ELEMENT("\n");
+
+
+    private final String element;
+
+    OutputElement(String element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        return element;
+    }
+}
