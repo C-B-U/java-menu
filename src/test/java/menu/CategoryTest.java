bd56796365f289dcb07e73453690a2166394e59f
GaBaljaintheroom <junsu1222@naver.com>
2023-08-30 19:48:56 +0900

    test: 추천 카테고리가 카테고리 수 만큼 저장이 되는지 확인

---
+++ src/test/java/menu/CategoryTest.java
@@ -0,0 +1,25 @@
+package menu;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+class CategoryTest {
+
+    RecommendCategory recommendCategory = new RecommendCategory();
+
+    @Test
+    @DisplayName("추천 카테고리가 카테고리 수 만큼 저장이 되는지 확인한다.")
+    void checkSaveCategories(){
+        //given
+        recommendCategory.recommendCategory();
+
+        //when
+        List<String> categories = recommendCategory.getCategories();
+
+        //then
+        assertThat(categories.size()).isEqualTo(5);
+    }
+}
