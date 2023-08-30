43150ccab82bb4625f8f20565ea4e8c7b2a190ac
GaBaljaintheroom <junsu1222@naver.com>
2023-08-31 01:24:04 +0900

    feat: Days outputDays 작성

--- src/main/java/menu/Days.java
+++ src/main/java/menu/Days.java
@@ -7,14 +7,21 @@ public enum Days {
     THURSDAY("목요일"),
     FRIDAY("금요일");
 
+    private static final String DIVISION = "구분";
     private final String value;
 
     Days(String value) {
         this.value = value;
     }
 
-    public String getValue() {
-        return value;
+    public static String outputDays() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(OutputElement.FIRST_ELEMENT).append(DIVISION);
+        for (Days days :Days.values()){
+            stringBuilder.append(OutputElement.MIDDLE_ELEMENT)
+                    .append(days.value);
+        }
+        stringBuilder.append(OutputElement.LAST_ELEMENT);
+        return stringBuilder.toString();
     }
-
 }
