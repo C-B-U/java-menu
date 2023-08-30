cd3d868d63c548207880afc18c734b60efc723c9
GaBaljaintheroom <junsu1222@naver.com>
2023-08-31 01:26:17 +0900

    feat: OutputView 추가

--- src/main/java/menu/OutputView.java
+++ src/main/java/menu/OutputView.java
@@ -13,4 +13,21 @@ public class OutputView {
     public void printCoachesForbiddenMenu(String name) {
         System.out.println(String.format(ProcessMessage.INPUT_FORBIDDEN_MENU.toString(), name));
     }
+
+    public void printStartMenu() {
+        System.out.println(ProcessMessage.OUTPUT_START_MENU);
+        printEnter();
+    }
+
+    public void printEnter(){
+        System.out.println();
+    }
+
+    public void printRecommendResult(RecommendMenu recommendMenu) {
+        System.out.println(ProcessMessage.OUTPUT_RESULT_MENU);
+        System.out.println(Days.outputDays());
+        System.out.println(Categories.outputCategories());
+        System.out.println(recommendMenu);
+        System.out.println(ProcessMessage.OUTPUT_FINISH_MENU);
+    }
 }
