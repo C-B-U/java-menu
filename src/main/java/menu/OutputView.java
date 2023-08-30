f7e734a5037b909792a12e749fc93ffbca06a775
GaBaljaintheroom <junsu1222@naver.com>
2023-08-30 16:26:53 +0900

    feat: OutputView printCoachesForbiddenMenu 작성

--- src/main/java/menu/OutputView.java
+++ src/main/java/menu/OutputView.java
@@ -9,4 +9,8 @@ public class OutputView {
     public void printErrorMessage(String error){
         System.out.println(error);
     }
+
+    public void printCoachesForbiddenMenu(String name) {
+        System.out.println(String.format(ProcessMessage.INPUT_FORBIDDEN_MENU.toString(), name));
+    }
 }
