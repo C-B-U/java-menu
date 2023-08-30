1ce2c9d1407b4b3d6970aeffd52d6666fd4ddf44
GaBaljaintheroom <junsu1222@naver.com>
2023-08-31 01:26:02 +0900

    feat: ProcessMessage 추가

--- src/main/java/menu/ProcessMessage.java
+++ src/main/java/menu/ProcessMessage.java
@@ -2,7 +2,10 @@ package menu;
 
 public enum ProcessMessage {
     INPUT_COACH_NAME("코치의 이름을 입력해 주세요. (, 로 구분)"),
-    INPUT_FORBIDDEN_MENU("%s(이)가 못 먹는 메뉴를 입력해 주세요.");
+    INPUT_FORBIDDEN_MENU("%s(이)가 못 먹는 메뉴를 입력해 주세요."),
+    OUTPUT_START_MENU("점심 메뉴 추천을 시작합니다."),
+    OUTPUT_RESULT_MENU("메뉴 추천 결과입니다."),
+    OUTPUT_FINISH_MENU("추천을 완료했습니다.");
 
     private final String message;
 
