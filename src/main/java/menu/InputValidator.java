9e3c80982eee99dc3ea4b3a9f33fa309d7b6b4ce
GaBaljaintheroom <junsu1222@naver.com>
2023-08-30 16:28:26 +0900

    feat: InputValidator validateForbiddenMenuCount 작성

--- src/main/java/menu/InputValidator.java
+++ src/main/java/menu/InputValidator.java
@@ -1,10 +1,19 @@
 package menu;
 
+import java.util.Arrays;
+
 public class InputValidator {
 
     public void validateCommaContain(String coachName){
         if (!coachName.contains(",")){
             throw new IllegalArgumentException(ErrorMessage.INPUT_COMMA_ERROR.toString());
         }
+    }
+
+    public void validateForbiddenMenuCount(String forbiddenMenu){
+         long forbiddenMenuCount = Arrays.stream(forbiddenMenu.split(",")).count();
+         if(forbiddenMenuCount > 2){
+             throw new IllegalArgumentException(ErrorMessage.INPUT_COMMA_ERROR.toString());
+         }
     }
 }
