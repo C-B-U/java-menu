bc9278ca95aeea4ec7e29d14bd95b5cd4cc58d99
GaBaljaintheroom <junsu1222@naver.com>
2023-08-31 11:59:08 +0900

    refactor: 상수 처리 추가

--- src/main/java/menu/view/validate/InputValidator.java
+++ src/main/java/menu/view/validate/InputValidator.java
@@ -6,15 +6,18 @@ import java.util.Arrays;
 
 public class InputValidator {
 
+    private static final String DIVISION = ",";
+    private static final int MAX_FORBIDDEN_MENU_COUNT = 2;
+
     public void validateCommaContain(String coachName){
-        if (!coachName.contains(",")){
+        if (!coachName.contains(DIVISION)){
             throw new IllegalArgumentException(ErrorMessage.INPUT_COMMA_ERROR.toString());
         }
     }
 
     public void validateForbiddenMenuCount(String forbiddenMenu){
          long forbiddenMenuCount = Arrays.stream(forbiddenMenu.split(",")).count();
-         if(forbiddenMenuCount > 2){
+         if(forbiddenMenuCount > MAX_FORBIDDEN_MENU_COUNT){
              throw new IllegalArgumentException(ErrorMessage.INPUT_COMMA_ERROR.toString());
          }
     }
