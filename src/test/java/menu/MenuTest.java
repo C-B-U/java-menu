29294e775ce8b423fb59e7f5306b200f9aac21a5
GaBaljaintheroom <junsu1222@naver.com>
2023-08-31 11:59:29 +0900

    test: type에 맞는 음식들이 반환되는지 확인

--- src/test/java/menu/MenuTest.java
+++ src/test/java/menu/MenuTest.java
@@ -1,11 +1,15 @@
 package menu;
 
+import menu.constant.Menus;
 import menu.model.CoachNames;
 import menu.model.ForbiddenMenu;
 import menu.model.RecommendCategory;
 import menu.model.RecommendMenu;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -25,5 +29,12 @@ class MenuTest {
         recommendMenu.createRecommendMenu(coachNames, forbiddenMenu, recommendCategory);
 
         assertThat(recommendMenu.getMenus().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("type에 맞는 음식들이 반환되는지 확인한다.")
+    void checkCorrectMappingMenu(){
+        List<String> menus = Menus.getFoods("일식");
+        assertThat(menus).isEqualTo(List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
     }
 }
