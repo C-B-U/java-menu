023bb0e1e5e02be0c331a7cc5d70a4ce7e05d499
김승진 <ohksj77@gmail.com>
2023-09-04 02:05:22 +0900

    feat: 코치 이름 입력 기능 적용
    기능 추가
    - 코치 이름 입력

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,12 +1,16 @@
 package menu.controller;
 
+import menu.domain.CoachList;
+import menu.io.InputManager;
 import menu.io.OutputView;
 
 public class MenuController {
     private final OutputView outputView;
+    private final InputManager inputManager;
 
-    public MenuController(final OutputView outputView) {
+    public MenuController(final OutputView outputView, final InputManager inputManager) {
         this.outputView = outputView;
+        this.inputManager = inputManager;
     }
 
     public void start() {
@@ -16,5 +20,6 @@ public class MenuController {
 
     private void createCoach() {
         outputView.printCoachNameRequest();
+        final CoachList coachList = inputManager.readCoach();
     }
 }
--- src/main/java/menu/factory/ComponentFactory.java
+++ src/main/java/menu/factory/ComponentFactory.java
@@ -1,12 +1,27 @@
 package menu.factory;
 
 import menu.controller.MenuController;
+import menu.io.InputManager;
+import menu.io.InputValidator;
+import menu.io.InputView;
 import menu.io.OutputView;
 
 public class ComponentFactory {
 
     public MenuController menuController() {
-        return new MenuController(outputView());
+        return new MenuController(outputView(), inputManager());
+    }
+
+    private InputManager inputManager() {
+        return new InputManager(inputView());
+    }
+
+    private InputView inputView() {
+        return new InputView(inputValidator());
+    }
+
+    private InputValidator inputValidator() {
+        return new InputValidator();
     }
 
     private OutputView outputView() {
