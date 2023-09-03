8ce087ba638711d33876b4429311ef37d329f567
김승진 <ohksj77@gmail.com>
2023-09-04 03:05:53 +0900

    feat: 코치 못먹는 음식 입력 기능 연결
    기능 추가
    - 코치의 못먹는 음식 입력 기능 연결

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,11 +1,17 @@
 package menu.controller;
 
-import menu.domain.CoachList;
+import menu.collection.CoachList;
+import menu.collection.CoachMenuMap;
 import menu.io.InputManager;
 import menu.io.OutputView;
 import menu.service.MenuService;
 
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class MenuController {
+    private static final int START_INDEX = 0;
     private final OutputView outputView;
     private final InputManager inputManager;
     private final MenuService menuService;
@@ -19,7 +25,14 @@ public class MenuController {
     public void start() {
         outputView.printStart();
         final CoachList coachList = createCoachList();
-        
+        final CoachMenuMap coachMenuMap = coachMenuInput(coachList);
+    }
+
+    private CoachMenuMap coachMenuInput(final CoachList coachList) {
+        return new CoachMenuMap(IntStream.range(START_INDEX, coachList.getCoachNum())
+                .mapToObj(i -> coachList.getNextCoach())
+                .peek(outputView::printCoachMenuRequest)
+                .collect(Collectors.toMap(Function.identity(), coach -> inputManager.readCoachMenu())));
     }
 
     private CoachList createCoachList() {
