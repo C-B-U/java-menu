0e6aab4b174e6375ae362ca55aafb56fa5de5bb5
GaBaljaintheroom <junsu1222@naver.com>
2023-09-17 15:11:24 +0900

    feat: Coaches 생성

---
+++ src/main/java/menu/Coaches.java
@@ -0,0 +1,35 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static menu.InputValidator.DIVISION;
+
+public class Coaches {
+
+    private static final int MIN_COACH_NUMBER = 2;
+    private static final int MAX_COACH_NUMBER = 5;
+    private final List<Coach> coaches = new ArrayList<>();
+
+    public Coaches(String names) {
+        String[] coachNames = names.split(DIVISION);
+        validateCoachNumber(coachNames.length);
+        addCoaches(coachNames);
+    }
+
+    private void validateCoachNumber(int number) {
+        if (number < MIN_COACH_NUMBER || number > MAX_COACH_NUMBER){
+            throw new IllegalArgumentException(ErrorMessage.COACH_NUMBER_ERROR.getMessage());
+        }
+    }
+
+    private void addCoaches(String[] coachNames) {
+        coaches.addAll(Arrays.stream(coachNames)
+                .map(String::trim)
+                .map(Coach::new)
+                .collect(Collectors.toList()));
+    }
+
+}
