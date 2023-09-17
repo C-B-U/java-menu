b5c5d0dbc920149ce85b7820b0233fe4e7d700e4
GaBaljaintheroom <junsu1222@naver.com>
2023-09-17 16:01:26 +0900

    feat: ProgressMessage START_MENU_RECOMMEND 추가

--- src/main/java/menu/ProgressMessage.java
+++ src/main/java/menu/ProgressMessage.java
@@ -1,6 +1,7 @@
 package menu;
 
 public enum ProgressMessage {
+    START_MENU_RECOMMEND("점심 메뉴 추천을 시작합니다."),
     INPUT_COACH_NAME("코치의 이름을 입력해 주세요. (, 로 구분)"),
     INPUT_COACH_FORBIDDEN_MENU("%s(이)가 못 먹는 메뉴를 입력해 주세요.");
     private final String message;
