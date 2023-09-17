d9641b0a111efa95ba6bb1f17d92333e77b1c8fe
GaBaljaintheroom <junsu1222@naver.com>
2023-09-17 15:11:34 +0900

    feat: OutputView 생성

---
+++ src/main/java/menu/OutputView.java
@@ -0,0 +1,12 @@
+package menu;
+
+public class OutputView {
+
+    public void printErrorMessage(String error){
+        System.out.println(error);
+    }
+
+    public void printProgressCoachNameMessage(){
+        System.out.println(ProgressMessage.INPUT_COACH_NAME);
+    }
+}
