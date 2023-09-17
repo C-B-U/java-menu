bf98464b533ea4d818bd84f3d13ea50c60276da6
GaBaljaintheroom <junsu1222@naver.com>
2023-09-17 20:42:44 +0900

    feat: MeProgressMessage 메세지 추가

--- src/main/java/menu/ProgressMessage.java
+++ src/main/java/menu/ProgressMessage.java
@@ -3,7 +3,9 @@ package menu;
 public enum ProgressMessage {
     START_MENU_RECOMMEND("점심 메뉴 추천을 시작합니다."),
     INPUT_COACH_NAME("코치의 이름을 입력해 주세요. (, 로 구분)"),
-    INPUT_COACH_FORBIDDEN_MENU("%s(이)가 못 먹는 메뉴를 입력해 주세요.");
+    INPUT_COACH_FORBIDDEN_MENU("%s(이)가 못 먹는 메뉴를 입력해 주세요."),
+    RECOMMEND_MENU_RESULT("메뉴 추천 결과입니다."),
+    FINISH_RECOMMEND("추천을 완료했습니다.");
     private final String message;
 
     ProgressMessage(String message) {
@@ -11,6 +13,11 @@ public enum ProgressMessage {
     }
 
     public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
         return message;
     }
 }
