d1c2237ea13389cce9bcf3e0c4c94d56f6783534
GaBaljaintheroom <junsu1222@naver.com>
2023-09-17 15:46:12 +0900

    feat: OutputView printProgressCoachForbiddenMessage 추가

--- src/main/java/menu/OutputView.java
+++ src/main/java/menu/OutputView.java
@@ -9,4 +9,8 @@ public class OutputView {
     public void printProgressCoachNameMessage(){
         System.out.println(ProgressMessage.INPUT_COACH_NAME);
     }
+
+    public void printProgressCoachForbiddenMessage(String name){
+        System.out.printf((ProgressMessage.INPUT_COACH_FORBIDDEN_MENU.getMessage()) + "%n", name);
+    }
 }
