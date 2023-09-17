74ac9d45e6973f03ac6911816c82137df59fb394
GaBaljaintheroom <junsu1222@naver.com>
2023-09-17 15:46:22 +0900

    feat: ProgressMessage INPUT_COACH_FORBIDDEN_MENU 추가

--- src/main/java/menu/ProgressMessage.java
+++ src/main/java/menu/ProgressMessage.java
@@ -1,7 +1,8 @@
 package menu;
 
 public enum ProgressMessage {
-    INPUT_COACH_NAME("코치의 이름을 입력해 주세요. (, 로 구분)");
+    INPUT_COACH_NAME("코치의 이름을 입력해 주세요. (, 로 구분)"),
+    INPUT_COACH_FORBIDDEN_MENU("%s(이)가 못 먹는 메뉴를 입력해 주세요.");
     private final String message;
 
     ProgressMessage(String message) {
