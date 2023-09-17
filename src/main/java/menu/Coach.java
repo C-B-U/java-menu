11d9ad55784081e7a4f2d0abc3fa3bc118468e71
GaBaljaintheroom <junsu1222@naver.com>
2023-09-17 15:11:18 +0900

    feat: Coach 생성

---
+++ src/main/java/menu/Coach.java
@@ -0,0 +1,21 @@
+package menu;
+
+public class Coach {
+
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final int MAX_NAME_LENGTH = 4;
+    private final String name;
+    public final ForbiddenMenu forbiddenMenu;
+
+    public Coach(String name) {
+        validateCoachNameLength(name);
+        this.name = name;
+        this.forbiddenMenu = new ForbiddenMenu();
+    }
+
+    private void validateCoachNameLength(String name) {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH){
+            throw new IllegalArgumentException(ErrorMessage.COACH_NAME_LENGTH_ERROR.getMessage());
+        }
+    }
+}
