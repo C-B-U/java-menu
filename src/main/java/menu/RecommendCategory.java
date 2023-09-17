60ddf2e449f7bdd920aaa1ddb7c7fe21e7e59354
GaBaljaintheroom <junsu1222@naver.com>
2023-09-17 20:43:52 +0900

    feat: RecommendCategory 메서드 추가

--- src/main/java/menu/RecommendCategory.java
+++ src/main/java/menu/RecommendCategory.java
@@ -10,15 +10,17 @@ public class RecommendCategory {
     private static final int MAX_DUPLICATE_COUNT = 2;
     private static final int MIN_CATEGORY_NUM = 1;
     private static final int MAX_CATEGORY_NUM = 5;
+    private static final String TAG = "카테고리";
+    private final List<String> categories = new ArrayList<>();
 
-    List<String> categories = new ArrayList<>();
 
-
-    public void randomCategory(){
+    public boolean isAddingRandomCategory(){
         String category = Categories.findCategory(Randoms.pickNumberInRange(MIN_CATEGORY_NUM, MAX_CATEGORY_NUM));
         if (canAddCategory(category)){
             categories.add(category);
+            return true;
         }
+        return false;
     }
 
     private boolean canAddCategory(String newCategory) {
@@ -26,5 +28,23 @@ public class RecommendCategory {
                 .filter(category -> category.equals(newCategory))
                 .count();
         return count < MAX_DUPLICATE_COUNT;
+    }
+
+    public boolean canRecommendCategory(){
+        return categories.size() < MAX_CATEGORY_NUM;
+    }
+
+    public String getDaysCategory(){
+        return categories.get(categories.size() - 1);
+    }
+
+    public String showRecommendCategory(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(ScreenElement.FIRST_ELEMENT).append(TAG);
+        for (String category : categories){
+            sb.append(ScreenElement.MIDDLE_DIVISION).append(category);
+        }
+        sb.append(ScreenElement.LAST_ELEMENT);
+        return sb.toString();
     }
 }
