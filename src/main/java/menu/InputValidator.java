40dd03671eb5ca80d6ac91bb1a7c53b3ac0e2e4e
GaBaljaintheroom <junsu1222@naver.com>
2023-09-17 15:45:38 +0900

    feat: InputValidator validateForbiddenMenuNumber 추가

--- src/main/java/menu/InputValidator.java
+++ src/main/java/menu/InputValidator.java
@@ -3,10 +3,18 @@ package menu;
 public class InputValidator {
 
     public static final String DIVISION = ",";
+    private static final int MAX_FORBIDDEN_MENU_SIZE = 2;
 
-    public void validateContainComma(String names){
-        if (!names.contains(DIVISION)){
+    public void validateContainComma(String names) {
+        if (!names.contains(DIVISION)) {
             throw new IllegalArgumentException(ErrorMessage.DIVISION_COMMA_ERROR.getMessage());
+        }
+    }
+
+    public void validateForbiddenMenuNumber(String menus) {
+        String[] menuNames = menus.split(DIVISION);
+        if (menuNames.length > MAX_FORBIDDEN_MENU_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.COACH_FORBIDDEN_MENU_NUMBER_ERROR.getMessage());
         }
     }
 }
