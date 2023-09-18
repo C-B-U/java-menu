f283e6b118c4fbcdeaf7492dfb055d3af2f78115
김승진 <ohksj77@gmail.com>
2023-09-19 00:04:45 +0900

    feat: 존재하는 메뉴인지 검증 기능

--- src/main/java/menu/constant/ErrorMessage.java
+++ src/main/java/menu/constant/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     INVALID_COACH_NAME_LENGTH("코치 이름 길이가 잘못되었습니다."),
     INVALID_COACH_NUM("코치의 인원수가 잘못되었습니다."),
     INVALID_INPUT_DELIMITER_USAGE("쉼표는 맨 처음이나 끝에 올 수 없습니다."),
-    INVALID_MENU_SIZE("못 먹는 메뉴의 개수가 잘못되었습니다.");
+    INVALID_MENU_SIZE("못 먹는 메뉴의 개수가 잘못되었습니다."),
+    INVALID_MENU_NAME("존재하지 않는 메뉴입니다.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -1,9 +1,19 @@
 package menu.domain;
 
+import menu.constant.ErrorMessage;
+import menu.constant.MenuCategory;
+
 public class Menu {
     private final String name;
 
     public Menu(final String name) {
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(final String name) {
+        if (MenuCategory.isNotExistMenu(name)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_MENU_NAME.getMessage());
+        }
     }
 }
