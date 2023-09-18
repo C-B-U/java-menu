3eecde0debcd6332d81112a9db888d699088588d
김승진 <ohksj77@gmail.com>
2023-09-18 23:09:50 +0900

    feat(OutputView): 시작 메시지 출력 기능

---
+++ src/main/java/menu/io/OutputView.java
@@ -0,0 +1,10 @@
+package menu.io;
+
+import menu.constant.GameMessage;
+
+public class OutputView {
+
+    public void printStart() {
+        System.out.println(GameMessage.START.getMessage());
+    }
+}
