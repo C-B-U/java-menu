f6482f03b4021532983c8d61e2b893f3fb0a9343
김승진 <ohksj77@gmail.com>
2023-09-18 23:35:16 +0900

    feat: 코치 입력값 저장소 클래스에 저장 기능

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,10 +1,15 @@
 package menu.controller;
 
+import menu.domain.Coaches;
+import menu.io.InputManager;
 import menu.io.OutputView;
+import menu.service.MenuService;
 
 public class MenuController {
 
     private final OutputView outputView = new OutputView();
+    private final InputManager inputManager = new InputManager();
+    private final MenuService menuService = new MenuService();
 
     public void run() {
         outputView.printStart();
@@ -13,5 +18,7 @@ public class MenuController {
 
     private void createCoach() {
         outputView.printCoachNameRequest();
+        final Coaches coaches = inputManager.readCoachNames();
+        menuService.saveCoaches(coaches);
     }
 }
